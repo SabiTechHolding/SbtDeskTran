9ef0189c3e72921805dfca90d75ba6b61b62ebc6
nambv-sbt <nambv@sabitech.vn>
2026-07-07 16:55:08 +0900

    feat: fix build version

--- main.py
+++ main.py
@@ -76,8 +76,9 @@ def save_settings(settings: dict):
 def main():
     settings = load_settings()
 
+    from version import __version__
     root = tk.Tk()
-    root.title("SbtDeskTran")
+    root.title(f"SbtDeskTran v{__version__}")
 
     # High-DPI awareness on Windows
     try:
